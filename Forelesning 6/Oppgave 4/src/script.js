6f0fb5dd0335fc842b148ddb76afc66c1719c6d3
crashable <sletbaklasse@gmail.com>
2020-09-28 17:07:05 +0200

    Endret slik at bakgrunn på menyen blir mørkere på mobil plattform

--- Forelesning 6/Oppgave 4/src/script.js
+++ Forelesning 6/Oppgave 4/src/script.js
@@ -4,8 +4,10 @@ toggleNav = (navName) => {
         height = document.getElementById(navName).style.height;
         if (height == "0%" || height == "") {
             document.getElementById(navName).style.height = "100%";
+            document.getElementById("footer-menu-popup").style.backgroundColor = "rgba(0,0,0, 0.8)";
         } else {
             document.getElementById(navName).style.height = "0%";
+            document.getElementById("footer-menu-popup").style.backgroundColor = "rgba(0,0,0, 0)";
         }
         console.log(document.getElementById(navName).style.height)
     }
